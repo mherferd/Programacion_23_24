3e642d55e2a43f43bb03bbdd87527b7b0866e08b
mherferd <mherferd@g.educaand.es>
2023-11-27 10:28:09 +0100

    ejercicios 27/11

--- ejercicios/src/main/java/U3/t4/ejercicio4.java
+++ ejercicios/src/main/java/U3/t4/ejercicio4.java
@@ -20,16 +20,40 @@ public class ejercicio4 {
         String letra = String.valueOf(pos.charAt(0));
         for (int j = 0; j < 8; j++) {
             if (letra.equalsIgnoreCase(columna[j])){
-            num=j;
+            num=j+1;
         }}
 
         String numero = String.valueOf(pos.charAt(1));
         for (int j = 0; j < 8; j++) {
-            if (letra.equalsIgnoreCase(fila[j])){
-            num2=j;
+            if (numero.equalsIgnoreCase(fila[j])){
+            num2=j+1;
         }}
+        int num_1=num;
+        int num_2=num2;
 
-        System.out.println(num2);
         System.out.println(num);
+        System.out.println(num2);
+
+
+        while (num_1+num_2<=14){
+            System.out.print(columna[num_1]);
+            System.out.println(fila[num_2]);
+            num_1++;
+            num_2++;
+        }
+
+        num_1=num;
+        num_2=num2;
+
+        while (num_1+num_2<=14){
+            System.out.print(columna[num_1);
+            System.out.println(fila[num_2]);
+            num_1++;
+            num_2++;
+        }
+
+
+
+
     }
 }
