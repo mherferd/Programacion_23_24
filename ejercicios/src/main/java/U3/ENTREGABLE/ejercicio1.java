6225fed5c4a1e2a62273f4bb45b995ae6c00de54
mherferd <mherferd@g.educaand.es>
2023-12-01 11:25:18 +0100

    ENTREGABLE 23-24 (MIGUEL HERNÁNDEZ)

--- ejercicios/src/main/java/U3/ENTREGABLE/ejercicio1.java
+++ ejercicios/src/main/java/U3/ENTREGABLE/ejercicio1.java
@@ -13,14 +13,36 @@ public class ejercicio1 {
         n = teclado.nextInt();
 
         int[] a = new int[n];
-        int[] b = new int[n];
-        int[] c = new int[n];
 
+        for (int j = 0; j < a.length; j++) {
+            a[j] = (int) (Math.random() *  47 +18);
+            System.out.print(a[j]);
+            System.out.print(" | ");}
 
+        System.out.println();
+        paresImpares(a);
+        int[] pares=paresImpares(a);
+        System.out.println(Arrays.toString(pares));
     }
 
-    public static void funcion(int contador){
-        System.out.println("A introducido un total de "+contador+" numeros impares");
+
+    public static int[] paresImpares(int a[]) {
+        int[] resultado = new int[a.length];
+        int pares = 0;
+        int impares = a.length - 1;
+
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] % 2 == 0) {
+                resultado[pares] = a[j];
+                pares++;
+            }}
+        for (int j = 0; j < a.length; j++) {
+        if (a[j] % 2 != 0) {
+            resultado[pares] = a[j];
+            pares++;
+        }}
+        return resultado;
     }
 }
+
 
--- ejercicios/src/main/java/U3/ENTREGABLE/ejercicio3.java
+++ ejercicios/src/main/java/U3/ENTREGABLE/ejercicio3.java
@@ -1,17 +1,74 @@
 package U3.ENTREGABLE;
 
-public class ejercicio3 {
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class ejercicio3 {
+    //SI QUIERE QUE EL RESULTADO SEA TRUE, COMO LAS POSIBILIDADES
+    // SON MUY BAJAS ES RECOMENDABLE CAMBIAR EL VALOR DE MATH.RANDOM A 1.
     public static void main(String[] args) {
 
-        int [][] datos = new int[5][5];
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el tamaño del array: ");
+        int n = teclado.nextInt();
+        int[][] datos = new int[n][n];
         for (int i = 0; i < datos.length; i++) {
             for (int j = 0; j < datos[i].length; j++) {
-                datos[i][j]= i+j;
+                datos[i][j] = (int) (Math.random() *  10)+1;
                 System.out.print(datos[i][j]);
-            }
+                System.out.print(" | ");}
+
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+
+        boolean resultado = cuadradoMagico(datos);
+
+        System.out.println(resultado);
+    }
+
+
+    public static boolean cuadradoMagico(int [][] datos) {
+        boolean variable=false;
+
+        int[] filas = new int [datos.length];
+
+        for (int i=0; i<datos.length;i++){
+            int suma=0;
+        for (int j = 0; j < datos.length; j++) {
+            suma=datos[1][j]+suma;
+        }
+            filas[i]=suma;
+        }
+
+
+
+        int[] columnas = new int [datos.length];
+
+        for (int j=0; j<datos.length;j++){
+            int suma=0;
+            for (int i = 0; i < datos.length; i++) {
+                suma=datos[i][j]+suma;
+            }
+            columnas[j]=suma;
+        }
+
+
+        int contador=0;
+        for (int j=0; j<filas.length; j++){
+            if (filas[j] == columnas[j]){
+            contador++;}
+
+        }
+
+        if (contador==filas.length){
+            return true;}
+
+
+        return variable;
 
     }
 }
